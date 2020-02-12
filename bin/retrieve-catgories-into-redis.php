d21de55fecb90cb438d8b861d409a13feaa54e3d
gisevevokoru <g+isevevokoru@commodea.com>
2020-02-12 16:37:27 +0000

    Add category retrieval script to bin and related util class to src/

---
+++ bin/retrieve-catgories-into-redis.php
@@ -0,0 +1,71 @@
+<?php declare(strict_types=1);
+use EzzeSiftuz\ProductsV1\Api\CategoriesApi;
+use EzzeSiftuz\ProductsV1\Model\CategoryGroups;
+use Kepawni\Limerick\Hexastore;
+use Predis\Client;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+//
+$config = RetrievalFunctions::readConfigFromCommandLine($argv);
+$client = RetrievalFunctions::createClientWithOAuthToken(RetrievalFunctions::requestNewOttoApiToken($config->market, $config->username, $config->phrase));
+$apiConfig = RetrievalFunctions::createProductApiConfiguration($config->market);
+$categoriesApi = new CategoriesApi($client, $apiConfig);
+$redisOptions = ['parameters' => ['database' => $config->{'redis-db-index'}, 'password' => $config->{'redis-secret'}]];
+[$redisHost, $redisPort] = explode(':', $config->redis . ':6379');
+$redis = new Client(sprintf("tcp://%s:%d", $redisHost, $redisPort), $redisOptions);
+$hexastore = new Hexastore($redis, $config->hexastore);
+//
+$getCategoryGroups = function (?int $page) use ($categoriesApi) {
+    return $categoriesApi->getCategoryGroups($page);
+};
+/** @var CategoryGroups $categoryGroups */
+foreach (RetrievalFunctions::iteratePages($getCategoryGroups) as $categoryGroups) {
+    foreach ($categoryGroups->getCategoryGroups() as $categoryGroup) {
+        $serializedCategoryGroup = serialize($categoryGroup);
+        $catGrpKey = 'cat-grp:' . sha1($serializedCategoryGroup);
+        $redis->set($catGrpKey, $serializedCategoryGroup);
+        $hexastore->store($catGrpKey, HexastorePredicate::BEARS_CATEGORY_GROUP_NAME, $categoryGroup->getCategoryGroup());
+        $attDefHashesByName = [];
+        foreach ($categoryGroup->getAttributes() as $attributeDefinition) {
+            $serializedAttributeDefinition = serialize($attributeDefinition);
+            $attDefKey = 'att-def:' . sha1($serializedAttributeDefinition);
+            $redis->set($attDefKey, $serializedAttributeDefinition);
+            $attDefHashesByName[$attributeDefinition->getName()] = $attDefKey;
+            $hexastore->store($catGrpKey, HexastorePredicate::DEFINES_ATTRIBUTE, $attDefKey);
+            $hexastore->store($attDefKey, HexastorePredicate::BEARS_ATTRIBUTE_NAME, $attributeDefinition->getName());
+            $hexastore->store($attDefKey, HexastorePredicate::BELONGS_TO_ATTRIBUTE_GROUP, $attributeDefinition->getAttributeGroup());
+            $hexastore->store($attDefKey, HexastorePredicate::HAS_ATTRIBUTE_TYPE, $attributeDefinition->getType());
+            if ($attributeDefinition->getUnit()) {
+                $hexastore->store($attDefKey, HexastorePredicate::AMOUNTS_ARE_GIVEN_IN, $attributeDefinition->getUnit());
+            }
+            if ($attributeDefinition->getUnitDisplayName()) {
+                $hexastore->store(
+                    $attDefKey,
+                    HexastorePredicate::AMOUNTS_ARE_DISPLAYED_WITH_UNIT_NAME,
+                    $attributeDefinition->getUnitDisplayName()
+                );
+            }
+            $hexastore->store(
+                $attDefKey,
+                HexastorePredicate::ALLOWS_MULTIPLE_VALUES,
+                $attributeDefinition->getMultiValue() ? 'true' : 'false'
+            );
+            $hexastore->store($attDefKey, HexastorePredicate::HAS_RELEVANCE, $attributeDefinition->getRelevance());
+            foreach ($attributeDefinition->getAllowedValues() ?: [] as $value) {
+                $hexastore->store($attDefKey, HexastorePredicate::ALLOWS_VALUE, $value);
+            }
+        }
+        foreach ($categoryGroup->getCategories() as $categoryName) {
+            $hexastore->store($catGrpKey, HexastorePredicate::CONTAINS_CATEGORY, $categoryName);
+        }
+        foreach ($categoryGroup->getVariationThemes() as $variationTheme) {
+            if ($attDefHashesByName[$variationTheme] ?? null) {
+                $hexastore->store(
+                    $attDefHashesByName[$variationTheme],
+                    HexastorePredicate::CREATES_VARIATION_WITHIN_CATEGORY_GROUP,
+                    $catGrpKey
+                );
+            }
+        }
+    }
+}
---
+++ src/RetrievalFunctions.php
@@ -0,0 +1,126 @@
+<?php declare(strict_types=1);
+use deswerve\colin\CommandLineInterface;
+use EzzeSiftuz\ProductsV1\Configuration;
+use GuzzleHttp\Client as HttpClient;
+
+class RetrievalFunctions
+{
+    public static function createClientWithOAuthToken(string $oAuthToken): HttpClient
+    {
+        return new HttpClient(['headers' => ['Authorization' => 'Bearer ' . $oAuthToken]]);
+    }
+
+    public static function createProductApiConfiguration(string $host): Configuration
+    {
+        $configuration = Configuration::getDefaultConfiguration();
+        $configuration->setHost(sprintf("https://%s", $host));
+        return $configuration;
+    }
+
+    /**
+     * To iterate over the responses of paged requests until there are no more pages, like for instance
+     *     $result = $fooApi->getBarGroups('quux', true, null, 100);
+     *     $result = $fooApi->getBarGroups('quux', true, 1, 100);
+     *     $result = $fooApi->getBarGroups('quux', true, 2, 100);
+     * you can use
+     *     $callback = function(?int $page) use($fooApi) {
+     *         return $fooApi->getBarGroups('quux', true, $page, 100);
+     *     }
+     *     foreach (iteratePages($callback) as $result)) {
+     *         // ...
+     *     }
+     * instead. The response needs to provide the getLinks() method, whose results will be scanned for the one with
+     * getRel() == 'next' which must respond to getHref() with a URI string featuring a page query parameter.
+     *
+     * @param callable $pageCallback
+     * @param int|null $startAt
+     *
+     * @return Generator
+     */
+    public static function iteratePages(callable $pageCallback, ?int $startAt = null): Generator
+    {
+        do {
+            $result = $pageCallback($startAt);
+            $startAt = null;
+            if (is_object($result) && method_exists($result, 'getLinks')) {
+                foreach ($result->getLinks() as $link) {
+                    if (is_object($link) && method_exists($link, 'getRel') && method_exists($link, 'getHref') && $link->getRel() === 'next') {
+                        parse_str(parse_url($link->getHref(), PHP_URL_QUERY), $data);
+                        $startAt = intval($data['page'] ?? null);
+                    }
+                }
+            }
+            yield $result;
+        } while ($startAt);
+    }
+
+    public static function readConfigFromCommandLine(array $argv): stdClass
+    {
+        $colin = new CommandLineInterface($argv[0], ['OPTIONS']);
+        $commandLine = $colin
+            ->addOption('redis', 'Host (and port if not 6379) to Redis server, e. g. "example.com:6379"', ['HOSTPORT'], 'r')
+            ->addOption('redis-db-index', 'Redis database index, default 0', ['NUM'], 'i', true, [0])
+            ->addOption('redis-secret', 'Redis secret key', ['PASSWORD'], 's', true, [null])
+            ->addOption('hexastore', 'Redis key of the sorted set holding the hexastore, default "hexastore"', ['NAME'], 'h', true, ['hexastore'])
+            ->addOption('market', 'Host (and port if not 443) to Market API server, e. g. "example.com"', ['HOSTPORT'], 'm')
+            ->addOption('username', 'Market user', ['NAME'], 'u')
+            ->addOption('phrase', 'Market password', ['PASSWORD'], 'p')
+            ->processCommandLine($argv);
+        try {
+            $options = (array)$commandLine->options;
+            $config = (object)array_combine(
+                array_keys($options),
+                array_map(
+                    function (string $key) use ($options) {
+                        $value = $options[$key]->values[0] ?? null;
+                        if ($key !== 'redis-secret' && is_null($value)) {
+                            throw new RuntimeException('Missing option ' . $key);
+                        }
+                        return $value;
+                    },
+                    array_keys($options)
+                )
+            );
+            if (!empty($commandLine->params)) {
+                throw new RuntimeException('Unsupported extra parameters found');
+            }
+            return $config;
+        } catch (RuntimeException $e) {
+            echo $e->getMessage(), PHP_EOL, PHP_EOL, $colin;
+            exit;
+        }
+    }
+
+    public static function requestNewOttoApiToken(string $hostPort, string $username, string $phrase): string
+    {
+        return json_decode(
+            file_get_contents(
+                sprintf('https://%s/v1/token', $hostPort),
+                false,
+                stream_context_create(
+                    [
+                        'http' => [
+                            'method' => 'POST',
+                            'header' => "Content-Type: application/x-www-form-urlencoded\r\nCache-Control: no-cache",
+                            'content' => http_build_query(
+                                [
+                                    'username' => $username,
+                                    'password' => $phrase,
+                                    'grant_type' => 'password',
+                                    'client_id' => 'token-otto-api',
+                                ],
+                                '',
+                                "&",
+                                PHP_QUERY_RFC3986
+                            ),
+                        ],
+                        'ssl' => ['verify_host' => true, 'allow_self_signed' => true],
+                    ]
+                )
+            ),
+            false,
+            2,
+            JSON_THROW_ON_ERROR
+        )->access_token;
+    }
+}
